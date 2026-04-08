2c8ba36bf5e9f6e48a854b2a93aae4f5f3160714
MatSH99 <51320263+MatSH99@users.noreply.github.com>
2026-04-08 19:50:53 +0200

    Rename environment variables for Tesseract

--- start.sh
+++ start.sh
@@ -6,15 +6,15 @@ MONITOR_DIR="./infrastructure/live"
 # --- 1. RETRIEVE DATA FROM TERRAGRUNT ---
 echo "Retrieve infrastructure information from Terragrunt..."
 
-export BUCKET_NAME=$(terragrunt output -raw --working-dir "tesseract/deployment/live/aws/test" s3_bucket_name)
-export DB_HOST=$(terragrunt output -raw --working-dir "tesseract/deployment/live/aws/test" rds_aurora_cluster_endpoint)
+export TESSERACT_BUCKET_NAME=$(terragrunt output -raw --working-dir "tesseract/deployment/live/aws/test" s3_bucket_name)
+export TESSERACT_DB_HOST=$(terragrunt output -raw --working-dir "tesseract/deployment/live/aws/test" rds_aurora_cluster_endpoint)
 export TESSERACT_SIGNER_ECDSA_P256_PUBLIC_KEY_ID=$(terragrunt output -raw --working-dir "tesseract/deployment/live/aws/test" ecdsa_p256_public_key_id)
 export TESSERACT_SIGNER_ECDSA_P256_PRIVATE_KEY_ID=$(terragrunt output -raw --working-dir "tesseract/deployment/live/aws/test" ecdsa_p256_private_key_id)
 
 # --- 2. RETRIEVE PASSWORD FROM SECRETS MANAGER ---
 echo "Retrieve database password..."
 SECRET_ARN=$(terragrunt output -json --working-dir "tesseract/deployment/live/aws/test" rds_aurora_cluster_master_user_secret | jq --raw-output .[0].secret_arn)
-export DB_PASSWORD=$(aws secretsmanager get-secret-value --secret-id "$SECRET_ARN" --query SecretString --output text | jq --raw-output .password)
+export TESSERACT_DB_PASSWORD=$(aws secretsmanager get-secret-value --secret-id "$SECRET_ARN" --query SecretString --output text | jq --raw-output .password)
 
 export DYNAMO_TABLE=$(terragrunt output -raw --working-dir "infrastructure/live" table_name)
 
